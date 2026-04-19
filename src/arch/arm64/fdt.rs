a2a645cacab0f08f63774392b4bd43f57a527c54
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-04-19 14:18:47 -0400

    dirty test

--- src/arch/arm64/fdt.rs
+++ src/arch/arm64/fdt.rs
@@ -6,5 +6,6 @@ pub const MAX_FDT_SZ: usize = 2 * 1024 * 1024;
 pub fn get_cmdline() -> Option<String> {
     let fdt = get_fdt();
 
-    Some(fdt.chosen()?.bootargs()?.to_string())
+    // Some(fdt.chosen()?.bootargs()?.to_string())
+    Some("--rootfs=ext4fs --automount=/dev,devfs --automount=/tmp,tmpfs --automount=/proc,procfs --automount=/sys,sysfs --init=/bin/ash --init-arg=-i".to_string())
 }
